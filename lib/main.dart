cbc4783c0a7b84abfba1c00dc7e8fde74dc16589
Cole.Akash <cole.akash@Akash.local>
2020-10-20 14:50:05 +0530

    Flex using Expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,62 +9,36 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello I am AKash '),
+        title: Text('My first App'),
         centerTitle: true,
         backgroundColor: Colors.lightBlue[400],
       ),
-
-      // body: Container(
-      //   padding: EdgeInsets.fromLTRB(10, 20, 50, 60),
-      //   margin: EdgeInsets.all(30),
-      //   color: Colors.grey[600],
-      //   child: Text('hello'),
-      // ),
-
-      // body: Row(
-      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //   crossAxisAlignment: CrossAxisAlignment.baseline,
-      //   children: <Widget>[
-      //     Text('hello'),
-      //     FlatButton(
-      //       onPressed: () {},
-      //       color: Colors.amber,
-      //       child: Text('click me'),
-      //     ),
-      //     Container(
-      //       color: Colors.cyan,
-      //       padding: EdgeInsets.all(20),
-      //       child: Text('Isniifjdsh'),
-      //     ),
-      //   ],
-      // ),
-
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      // Flex Box Expanded
+      body: Row(
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text('hello'),
-              Text('World'),
-              Container(
-                padding: EdgeInsets.all(20),
-                color: Colors.amber,
-                child: Text('One'),
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                color: Colors.pink,
-                child: Text('two'),
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                color: Colors.blue,
-                child: Text('three'),
-              )
-            ],
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(30),
+              color: Colors.pink,
+              child: Text('Hi'),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(30),
+              color: Colors.lightBlue,
+              child: Text('how'),
+            ),
+          ),
+          Expanded(
+            flex: 6,
+            child: Container(
+              padding: EdgeInsets.all(30),
+              color: Colors.blueGrey,
+              child: Text('you?'),
+            ),
           ),
         ],
       ),
